7f9fd2bcecf4a5daf93d7addf4f19df5b0b5748e
Andrean Franc <andrean.franc@gmail.com>
2015-12-18 14:49:11 +0100

    Add local timestamp column and convert both timestamps to naive datetime objects before insertion

--- analytics/analytics/tasks.py
+++ analytics/analytics/tasks.py
@@ -1,10 +1,16 @@
 import logging
 
+from dateutil.tz import tzoffset
+
 from .bitstream import bytes_to_hex
 from .data import StatBitStream, DEVICE_ID_LENGTH
 
 
 ANALYTICS_TABLE = 'stats'
+
+
+def to_seconds(hours):
+    return hours * 60 * 60
 
 
 def process_stats(supervisor, data):
@@ -14,6 +20,13 @@ def process_stats(supervisor, data):
     for entry in stats:
         data = dict(device_id=device_id)
         data.update(entry)
+        utc_timestamp = data['timestamp']
+        local_tz = tzoffset(None, to_seconds(data['timezone']))
+        # converting both timestamps into naive datetime objects because
+        # postgres does not perform automatic conversion
+        data['local_timestamp'] = (utc_timestamp.astimezone(local_tz)
+                                                .replace(tzinfo=None))
+        data['timestamp'] = utc_timestamp.replace(tzinfo=None)
         q = db.Insert(ANALYTICS_TABLE, cols=data.keys())
         db.execute(q, data)
     logging.debug("%s stat entries stored from %s", len(stats), device_id)
